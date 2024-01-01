226299fb2daad7694ea71a4ead65a3708379eece
cant-code <ayushc13000@gmail.com>
2024-01-01 17:39:17 +0530

    Init

---
+++ webapp/src/main/java/com/damnation/yt/filemanagement/webapp/YtFileManagementApplication.java
@@ -0,0 +1,13 @@
+package com.damnation.yt.filemanagement.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtFileManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtFileManagementApplication.class, args);
+    }
+
+}
